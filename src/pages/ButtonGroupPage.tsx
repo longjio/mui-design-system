f083ead4af4e38eb5910ea3c56c36febecbf4c07
jiokim <jiokim@shinsegae.com>
2025-05-27 16:14:23 +0900

    Button Group 수정

--- src/pages/ButtonGroupPage.tsx
+++ src/pages/ButtonGroupPage.tsx
@@ -1,37 +1,67 @@
 import * as React from 'react';
-import { Button, Stack, Typography, ButtonGroup } from '@mui/material';
+import { Button, Stack, Box, Typography, ButtonGroup } from '@mui/material';
+
+const buttons = [
+    <Button key="one">One</Button>,
+    <Button key="two">Two</Button>,
+    <Button key="three">Three</Button>,
+];
 
 export default function BasicButtonGroup() {
     return (
         <Stack spacing={3}>
-            <Typography variant="h6">Contained Buttons</Typography>
-            <ButtonGroup variant="contained" aria-label="Basic button group">
-                <Button>One</Button>
-                <Button>Two</Button>
-                <Button>Three</Button>
-            </ButtonGroup>
-
-            <Stack
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    '& > *': {
-                        m: 1,
-                    },
-                }}
-            >
+            <Box m={0} p={0}>
+                <Typography variant="h6">Basic button group</Typography>
+                <ButtonGroup variant="contained" aria-label="basic button group">
+                    <Button>One</Button>
+                    <Button>Two</Button>
+                    <Button>Three</Button>
+                </ButtonGroup>
+            </Box>
+            <Stack>
+                <Typography variant="h6">Button variants</Typography>
                 <ButtonGroup variant="outlined" aria-label="Basic button group">
                     <Button>One</Button>
                     <Button>Two</Button>
                     <Button>Three</Button>
                 </ButtonGroup>
+            </Stack>
+            <Stack>
                 <ButtonGroup variant="text" aria-label="Basic button group">
                     <Button>One</Button>
                     <Button>Two</Button>
                     <Button>Three</Button>
                 </ButtonGroup>
             </Stack>
+            <Stack spacing={3}>
+                <Typography variant="h6">Sizes and colors</Typography>
+                <ButtonGroup size="small" aria-label="Small button group">
+                    {buttons}
+                </ButtonGroup>
+                <ButtonGroup color="secondary" aria-label="Medium-sized button group">
+                    {buttons}
+                </ButtonGroup>
+                <ButtonGroup size="large" aria-label="Large button group">
+                    {buttons}
+                </ButtonGroup>
+            </Stack>
+            <Typography variant="h6">Sizes and colors</Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    '& > *': {
+                        m: 1,
+                    },
+                }}
+            >
+                <ButtonGroup
+                    orientation="vertical"
+                    aria-label="Vertical button group"
+                    variant="text"
+                >
+                    {buttons}
+                </ButtonGroup>
+            </Box>
         </Stack>
     );
 }
